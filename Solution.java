ecfd58817642b270c52b26756a242b4003e7e621
xka119 <xka119@naver.com>
2018-08-03 20:56:21 +0900

    init Solution

--- Solution.java
+++ Solution.java
@@ -1,26 +1,11 @@
+
 /*
-3975. 승률 비교하기
+ 모든 테스트는 여기서 진행
 
-실행속도가 인터넷의 영향을 많이 받는건가??
-집에가서 테스트해보기 8/3
-*/
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+ */
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args){
 
-        int t = Integer.parseInt(br.readLine().trim());
-        for(int tc=1; tc<=t; tc++){
 
-        }
-        System.out.print(sb.toString());
-        br.close();
     }
-
-
 }
