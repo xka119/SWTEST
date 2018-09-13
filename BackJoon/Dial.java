63bd56af25ae4fffd7c69568cf21c91d5bdc387f
xka119 <xka119@naver.com>
2018-09-14 01:37:30 +0900

    5622. 다이얼 10828. 스택

---
+++ BackJoon/Dial.java
@@ -0,0 +1,50 @@
+package BackJoon;
+
+
+/*
+5622. 다이얼
+*/
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dial {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        String s = br.readLine();
+
+        int sum = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char t = s.charAt(i);
+            if(t=='A'||t=='B'||t=='C'){
+                sum += 3;
+            }else if(t=='D'||t=='E'||t=='F'){
+                sum +=4;
+            }else if(t=='G'||t=='H'||t=='I'){
+                sum +=5;
+            }else if(t=='J'||t=='K'||t=='L'){
+                sum +=6;
+            }else if(t=='M'||t=='N'||t=='O'){
+                sum +=7;
+            }else if(t=='P'||t=='Q'||t=='R'||t=='S'){
+                sum +=8;
+            }else if(t=='T'||t=='U'||t=='V'){
+                sum +=9;
+            }else if(t=='W'||t=='X'||t=='Y'||t=='Z'){
+                sum +=10;
+            }else{
+
+            }
+        }
+        System.out.print(sum);
+
+
+    }
+}
---
+++ BackJoon/Stack.java
@@ -0,0 +1,89 @@
+package BackJoon;
+
+
+/*
+10828. 스택
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Stack {
+    //정적 스택
+    static int index;
+    static int[] stack = new int[100000];
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        int count;
+        int n = Integer.parseInt(br.readLine().trim());
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            String s;
+
+            if(st.countTokens()==2){
+                s = st.nextToken();
+                count = Integer.parseInt(st.nextToken());
+                push(count);
+            }else{
+                s = st.nextToken();
+                if(s.equals("pop")){
+                    pop();
+                }else if(s.equals("size")){
+                    size();
+                }else if(s.equals("empty")){
+                    empty();
+                }else if(s.equals("top")){
+                    top();
+                }else{
+                    //nothing
+                }
+            }
+        }
+
+    }
+
+    //stack이란 뭔가
+    // 마지막 인덱스를 기준으로 FI LO 터벅터벅 쌓이는거임. 그니까 마지막 index를 가리키는 것이 필요함.
+
+    //push
+    public static void push(int x){
+        stack[index++] = x;
+    }
+
+    //pop
+    public static void pop(){
+        if(index==0){
+            System.out.println("-1");
+        }else{
+            System.out.println(stack[--index]);
+        }
+    }
+
+    //size
+    public static void size(){
+        System.out.println(index);
+    }
+
+    //empty 비었으면 1, 안비었으면 0
+    public static void empty(){
+        if(index==0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+
+    //top
+    public static void top(){
+        if(index==0){
+            System.out.println("-1");
+        }else{
+            System.out.println(stack[index-1]);
+        }
+    }
+}
