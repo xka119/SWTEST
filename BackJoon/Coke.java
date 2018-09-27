46145ac3a4161a2ee7b1e0819f34fe52700ccee1
xka119 <xka119@naver.com>
2018-09-27 21:07:35 +0900

    5032. 탄산 음료

---
+++ BackJoon/Coke.java
@@ -0,0 +1,36 @@
+package BackJoon;
+
+
+/*
+5032. 탄산 음료
+
+*/
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Coke {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int e, f, c;
+
+        String[] ss = br.readLine().split(" ");
+        e = Integer.parseInt(ss[0]);
+        f = Integer.parseInt(ss[1]);
+        c = Integer.parseInt(ss[2]);
+
+        int bottle = e + f;
+        int sum = 0;
+        int nbottle;
+        while (bottle >= c) {
+            nbottle = bottle/c;
+            bottle = nbottle + bottle%c;
+            sum += nbottle;
+        }
+
+        System.out.print(sum);
+
+    }
+}
