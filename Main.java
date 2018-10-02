0e267baccf251687087dc69a98089d5d899c5b78
xka119 <xka119@naver.com>
2018-10-02 13:00:50 +0900

    Main & Solution delete

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
 
-/*
- BackJoon
 
-*/
+/*
+2178. 미로 탐색
+
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 
 public class Main {
-    public static void main(String args[]) {
+    static class game{
+        int x,y;
+        public game(int x, int y){
+            this.x =x;
+            this.y = y;
+        }
+        public game(){
+        }
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // Test
+        game a = new game();
+
+
 
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,14 +1,44 @@
 
 /*
- SW Expert
+2178. 미로 탐색
 
 */
 
 
-public class Solution{
-    public static void main(String args[]) throws Exception {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
-        // Test
+public class Solution {
+    public int solution(int[] A) {
+        int count0 = 0;
+
+        for(int i=0; i<A.length; i++){
+            if(A[i] == 0)
+                count0++;
+        }
+        int count1 = A.length- count0;
+
+        if(count1>count0)
+            return count0;
+        else
+            return count1;
 
     }
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Solution s = new Solution();
+
+        int[] A = {1,0,0,1,0,0};
+
+        System.out.println(s.solution(A));
+
+
+    }
+
+
 }
